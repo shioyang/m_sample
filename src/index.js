d32e15f9c9e9090c9716f1528301148a6e5681e1
shioyang <shioyang4321@gmail.com>
2017-02-21 09:29:28 +0900

    Construct tag structure

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ var m = require("mithril");
 
 var root = document.body;
 
-m.render(root, m("h1", { class: "title" }, "Mithril Sample"));
+m.render(root, m("div", [
+    m("h1", { class: "title" }, "Mithril Sample"),
+    m("div", { class: "main" }, [
+        m("button", {}, "Click")
+    ])
+]));
